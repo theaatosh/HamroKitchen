1becb89240ba5302f927ad2076f0c93125dd8c8c
Aatosh <sharma.aatosh1416@gmail.com>
2024-10-21 09:50:31 +0545

    khalti photo added

--- Client/src/Pages/Payment.jsx
+++ Client/src/Pages/Payment.jsx
@@ -1,10 +1,20 @@
 import styles from '../Styles/Payment/Payment.module.css';
 export const Payment=()=>{
 
+    const handlePayment=()=>{
+        console.log('hello');
 
+
+    }
     return(
         <div className={styles.payment_container}>
             <h1>Payment Page</h1>
+            <p className={styles.subheading}>Select your preferred payment method:</p>
+            <div
+          className={`${styles.option} `}>
+          <img src='/Images/khaltipay.png' alt="Khalti" className={styles.payment_logo} onClick={handlePayment}/>
+          <p>Pay with Khalti</p>
+        </div>
 
         </div>
     )
