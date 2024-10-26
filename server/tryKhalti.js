c695167061a3cf9cba2422d8462d41a602004dbb
Pablo55-star <aneshkarki.077@kathfoed.edu.np>
2024-10-26 21:33:17 +0545

    bhayoherdim

--- server/tryKhalti.js
+++ server/tryKhalti.js
@@ -1,5 +1,7 @@
 const axios=require('axios');
 require('dotenv').config();
+const cors = require('cors');
+
 
 const khalti= async (req,res)=>{
     try{
@@ -25,11 +27,13 @@ const khalti= async (req,res)=>{
     );
     // console.log(response.data);
     console.log(response.data.payment_url);
-    res.json({
-        message: "khalti success",
-        payment_method: "khalti",
-        data: response.data,
-      });
+    // res.json({
+    //     message: "khalti success",
+    //     payment_method: "khalti",
+    //     data: response.data,
+    //   });
+    req.khalti=response.data;
+    next();
     }catch(err){
         console.log(err);
         res.send(err);
@@ -37,11 +41,18 @@ const khalti= async (req,res)=>{
 }
 
  const khaltiCallback=async(req,res)=>{
-//     const headers={
-//         Authorization: `Key ${process.env.KHALTI_SECRET_KEY}`,
-//        "Content-Type": "application/json",
-//     };
-//     const response = await axios.post("https://a.khalti.com/api/v2/epayment/lookup/", { pidx },{headers});
-//     console.log(response.data);
+    console.log("here");
+    if (!req.khalti || !req.khalti.pidx) {
+        console.log("khalti not init");
+        return res.status(400).json({ message: 'Khalti payment not initiated properly' });
+    }
+    const{pidx}=req.khalti.pidx;
+    const headers={
+        Authorization: `Key ${process.env.KHALTI_SECRET_KEY}`,
+       "Content-Type": "application/json",
+    };
+    const response = await axios.post("https://a.khalti.com/api/v2/epayment/lookup/", { pidx },{headers});
+    console.log(response.data);
+    res.send("heelo");
  }
 module.exports={khalti,khaltiCallback};
