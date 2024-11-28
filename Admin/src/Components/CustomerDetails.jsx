fb6108e3c88072265533417d48e1080b1dd26ffc
Aatosh <sharma.aatosh1416@gmail.com>
2024-11-28 20:52:11 +0545

    userDetail in process

---
+++ Admin/src/Components/CustomerDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CustomerDetails = () => {
+  return (
+    <div>
+      <h1>Customer Details</h1>
+    </div>
+  )
+}
+
---
+++ Admin/src/Components/KitchenDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const KitchenDetails = () => {
+  return (
+    <div>
+      <h1>Kitchen Details</h1>
+    </div>
+  )
+}
+
