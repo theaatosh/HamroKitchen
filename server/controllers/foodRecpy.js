f390d06c489736da0bb3fa0df4693d2278e08f5c
AneshKarki <aneshkarki07@gmail.com>
2025-01-14 12:44:46 +0545

    h

--- server/controllers/foodRecpy.js
+++ server/controllers/foodRecpy.js
@@ -1,25 +1,25 @@
-const user=require("../models/index");
-const {runPythonFunction}=require("../algorithm/test")
+const user = require("../models/index");
+const { runPythonFunction } = require("../algorithm/test");
 
-const foodRec=async (req,res)=>{
-    const {userId}=req.user;
-    // console.log("hi")
-    try{
-        console.log(userId);
-        const rec=await user.findById(userId,{_id:0, recData:1});
-        const recData=Object.values(rec.recData);
-        const data=await runPythonFunction(recData);
-        const finalData = data
-        .replace(/[\[\]']/g, '') 
-        .split(',') 
-        .map(id => id.trim());
-        console.log("this:"+finalData);
+const foodRec = async (req, res) => {
+  const { userId } = req.user;
+  // console.log("hi")
+  try {
+    console.log(userId);
+    const rec = await user.findById(userId, { _id: 0, recData: 1 });
+    const recData = Object.values(rec.recData);
+    const data = await runPythonFunction(recData);
+    // const finalData = data
+    // .replace(/[\[\]']/g, '')
+    // .split(',')
+    // .map(id => id.trim());
+    // console.log("this:"+finalData);
 
-        // console.log(typeof(finalData))
-        res.json(finalData);
-    }catch(err){
-        console.log(err);
-    }
-}
+    // console.log(typeof(finalData))
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-module.exports={foodRec};
+module.exports = { foodRec };
